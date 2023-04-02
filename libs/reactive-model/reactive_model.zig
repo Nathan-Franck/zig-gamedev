a1b1034b990902115f4078d18c36e5fabbbfb308
Nathan Franck <nathanfranck1@gmail.com>
2023-04-01 23:51:27 -0600

    Super naive reactive chaining now possible! Ideally would work in-concert with a event system to minimize redundant calculation

--- libs/reactive-model/reactive_model.zig
+++ libs/reactive-model/reactive_model.zig
@@ -1,40 +1,63 @@
 const std = @import("std");
 
-fn ReactiveModel(comptime T: type) type {
+// Given a type T, allow systems to register to changes of the model.
+// If a system requests a pointer to a member of the model, we should inform
+// all down-stream systems of a possible change.
+fn ReactiveModel(comptime T: type, comptime systems: anytype) type {
     return struct {
         const Self = @This();
-        value: T,
-        fn set(self: *Self, value: T) void {
-            self.value = value;
+        model: T,
+        fn init(model: T) Self {
+            return Self{ .model = model };
         }
-        fn init(value: T) Self {
-            return Self{ .value = value };
+        fn set(self: *Self, partial_model: anytype) void {
+            inline for (@typeInfo(@TypeOf(partial_model)).Struct.fields) |field| {
+                @field(self.model, field.name) = @field(partial_model, field.name);
+                inline for (systems) |system| {
+                    if (@hasField(system, field.name)) {
+                        const SystemValues = @typeInfo(@TypeOf(system.listen)).Fn.params[0].type.?;
+                        var system_values: SystemValues = undefined;
+                        inline for (@typeInfo(SystemValues).Struct.fields) |system_field| {
+                            switch (@typeInfo(system_field.type)) {
+                                .Pointer => {
+                                    @field(system_values, system_field.name) = &@field(self.model, system_field.name);
+                                },
+                                else => {
+                                    @field(system_values, system_field.name) = @field(self.model, system_field.name);
+                                },
+                            }
+                        }
+                        system.listen(system_values);
+                    }
+                }
+            }
         }
-        fn register(self: Self, comptime system: type) void {
-            std.debug.print("registering {*}\n", .{ @typeName(system) });
-            const Args = @typeInfo(@TypeOf(system.listen)).Fn.params[0].type.?;
-            const args = Args{ .foo = self.value.foo };
-            system.listen(args);
-        }        
     };
 }
 
-const test_model = struct { foo: u32 };
+const test_model = struct {
+    foo: u32,
+    bar: f32,
+};
 
 const test_system = struct {
     foo: u32,
+    bar: *f32,
     fn listen(thinger: @This()) void {
-        std.debug.print("foo = {}\n", .{thinger.foo});
+        thinger.bar.* = @intToFloat(f32, thinger.foo);
+        std.debug.print("bar = {}\n", .{thinger.bar.*});
     }
 };
 
 test "reactive model" {
-    var model = ReactiveModel(test_model).init(.{ .foo = 0 });
-    model.set(.{ .foo = 1 });
-    try std.testing.expect(model.value.foo == 1);
+    var model = ReactiveModel(test_model, .{}).init(.{ .foo = 0, .bar = 0.0 });
+    model.set(.{ .foo = 1, .bar = 1.0 });
+    try std.testing.expect(model.model.foo == 1);
 }
 
 test "register" {
-    var model = ReactiveModel(test_model).init(.{ .foo = 0 });
-    model.register(test_system);
+    var model = ReactiveModel(test_model, .{ test_system })
+        .init(.{ .foo = 123, .bar = 456.0 });
+    model.set(.{ .foo = 3, .bar = 1.0 });
+    std.debug.print("foo = {}\n", .{model.model.foo});
 }
